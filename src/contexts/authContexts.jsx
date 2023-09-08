52ea3dbcbe492fbba338a8af7c0451da7a3a1c54
David Vicent <djassiell@gmail.com>
2023-09-08 01:30:03 -0400

    fix context localstorage

--- src/contexts/authContexts.jsx
+++ src/contexts/authContexts.jsx
@@ -10,8 +10,7 @@ import {
 } from "react";
 
 const AUTH_TOKENS_KEY = "NEXT_JS_AUTH";
-const PERSONAJE_KEY = 'PERSONAJES_LOCAL'
-
+const PERSONAJE_KEY = "PERSONAJES_LOCAL";
 
 export const AuthContext = createContext({
   login: (authTokens) => {},
@@ -20,8 +19,10 @@ export const AuthContext = createContext({
   authTokens: null,
 });
 
-export default function AuthContextProvider({children}) {
-  const authTokensInLocalStorage = window.localStorage.getItem(AUTH_TOKENS_KEY);
+export default function AuthContextProvider({ children }) {
+  const authTokensInLocalStorage =
+    typeof window !== "undefined" &&
+    window.localStorage.getItem(AUTH_TOKENS_KEY);
   const [authTokens, setAuthTokens] = useState(
     authTokensInLocalStorage === null
       ? null
@@ -29,13 +30,16 @@ export default function AuthContextProvider({children}) {
   );
 
   const login = useCallback(function (authTokens) {
-    window.localStorage.setItem(AUTH_TOKENS_KEY, JSON.stringify(authTokens));
+    typeof window !== "undefined" &&
+      window.localStorage.setItem(AUTH_TOKENS_KEY, JSON.stringify(authTokens));
     setAuthTokens(authTokens);
   }, []);
 
   const logout = useCallback(function () {
-    window.localStorage.removeItem(AUTH_TOKENS_KEY);
-    window.localStorage.removeItem(PERSONAJE_KEY);
+    typeof window !== "undefined" &&
+      window.localStorage.removeItem(AUTH_TOKENS_KEY);
+    typeof window !== "undefined" &&
+      window.localStorage.removeItem(PERSONAJE_KEY);
     setAuthTokens(null);
   }, []);
 
